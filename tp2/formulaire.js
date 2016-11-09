1672484c7250856bfc58527495740d329c4a04f8
diarraseck <seckdiarra91@hotmail.com>
2016-11-09 21:48:00 +0100

    Add files via upload

---
+++ tp2/formulaire.js
@@ -0,0 +1,55 @@
+function validation()
+{
+	
+	if ( (document.formulaire.nom.value =="") || (document.formulaire.nom.value.length < 5 ))
+    {
+		document.getElementById("error").innerHTML="La saisie du nom est obligatoire";
+	
+		valid =false;
+	return valid;
+
+	}
+	
+	if (( document.formulaire.prenom.value =="") || (document.formulaire.prenom.value.length < 5 ))
+    {
+		document.getElementById("error").innerHTML="La saisie du prenom est obligatoire";
+	
+		valid =false;
+	return valid;
+
+	}
+	
+	if (( document.formulaire.naissance.value =="") || (document.formulaire.naissance.value.length < 5 ))
+    {
+		document.getElementById("error").innerHTML="La saisie de la date de naissance est obligatoire";
+	
+		valid =false;
+	return valid;
+
+	}
+	
+	if (( document.formulaire.adresse.value =="") || (document.formulaire.adresse.value.length < 5 ))
+    {
+		document.getElementById("error").innerHTML="La saisie de l'adresse est obligatoire";
+	
+		valid =false;
+	return valid;
+
+	}
+	
+	if (( document.formulaire.mail.value =="") || (document.formulaire.mail.value.length < 5 ))
+    {
+		document.getElementById("error").innerHTML="La saisie du mail est obligatoire";
+	
+		valid =false;
+	return valid;
+
+	}
+	
+	else
+	{
+	document.getElementById("resultat").innerHTML= "Bienvenue Diarra";
+	return true;
+	}
+	
+}
